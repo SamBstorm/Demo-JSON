16a63c8c02d290ce01210dbb9fa1cace2bcb9779
Legrain Samuel <samuel.legrain@bstorm.be>
2022-08-04 13:48:24 +0200

    Ajoutez des fichiers projet.

---
+++ Demo-JSON/FauxDictionnaire.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_JSON
+{
+    public class FauxDictionnaire
+    {
+        public string zero { get; set; } 
+        public string un { get; set; } 
+        public string deux { get; set; } 
+        public string trois { get; set; }
+    }
+}
---
+++ Demo-JSON/Program.cs
@@ -0,0 +1,32 @@
+﻿using Demo_JSON;
+using System.Text.Json;
+
+string message = "Hello world!";
+object objetRandom = new { Id = 1, Name = "Samuel", Mail = "info@test.com" };
+int[] ints = new int[] { 1, 2, 3 };
+object? nullAbsolu = null; 
+Dictionary<string, string> dic = new Dictionary<string, string>();
+dic.Add("zero", "Toto");
+dic.Add("un", "Titi");
+dic.Add("deux", "Tutu");
+dic.Add("trois", "Tata");
+
+FauxDictionnaire fauxDictionnaire = new FauxDictionnaire() { zero = "Toto", un="Titi", deux="Tutu", trois="Tata" };
+
+
+
+Console.WriteLine(JsonSerializer.Serialize(message));
+Console.WriteLine(JsonSerializer.Serialize(objetRandom));
+Console.WriteLine(JsonSerializer.Serialize(ints));
+Console.WriteLine(JsonSerializer.Serialize(nullAbsolu));
+Console.WriteLine(JsonSerializer.Serialize(dic));
+Console.WriteLine(JsonSerializer.Serialize(fauxDictionnaire));
+
+string json = JsonSerializer.Serialize(dic);
+
+fauxDictionnaire = JsonSerializer.Deserialize<FauxDictionnaire>(json);
+Console.WriteLine();
+
+
+object inconnu = JsonSerializer.Deserialize<FauxDictionnaire>("null");
+Console.WriteLine();
